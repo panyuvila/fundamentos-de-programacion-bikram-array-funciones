e56aed57beac19e08fd3688d31d3f10878b45477
panyuvila <73607368+panyuvila@users.noreply.github.com>
2022-02-21 09:40:56 +0100

    arreglos

---
+++ index.js
@@ -0,0 +1,10 @@
+const arrayVacio = []
+}
+const  arrayNumeros declarada = [0,1,2,3,4,5,6,7,8,9]
+}
+const arrayNumerosPares declarada = [0,2,4,6,8]
+}
+const arrayBidimensional declarada 
+}
+var arrayBidimensional declarada = [[0, 1, 2], ['a', 'b', 'c']]
+}
